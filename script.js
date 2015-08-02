26305d10972ec1b36f9579b9c63734cdae0c9a7f
yvonnel098 <lemonginger333@yahoo.com>
2015-08-02 12:02:48 -0700

    Erase FCKG_USE cookie in specific namespace so that it will not overwrite the same cookie created in root path

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
+//updated 8-2-15 to erase FCKG_USE cookie in specific namespace so
+//that it will not overwrite the same cookie created in root path
 
+function createCookie(name,value,days, cur_path) {
+    //console.log("in create cookie "+name);
 
-function createCookie(name,value,days) {
     if (days) {
         var date = new Date();
         date.setTime(date.getTime()+(days*24*60*60*1000));
         var expires = "; expires="+date.toGMTString();
     }
     else var expires = "";
-    document.cookie = name+"="+value+expires+"; path=/";
+    //document.cookie = name+"="+value+expires+"; path=/"+cur_path;
+    document.cookie = name+"="+value+expires+"; path="+cur_path;
 }
 
 function readCookie(name) {
@@ -21,16 +25,17 @@ function readCookie(name) {
     return null;
 }
 
-function eraseCookie(name) {
-    createCookie(name,"",-1, "/dokuwiki");
+function eraseCookie(name, path) {
+    createCookie(name,"",-1, path);
 }
 
 function set_ckg_edit() {
     //expire setedit cookie, if any
     //create new setedit cookie with CKG edit as the preference
     
-    eraseCookie("setedit");
-    createCookie("setedit", 'CKG', '365');
+    //console.log("in set_ckg_edit");
+    eraseCookie("setedit", "/");
+    createCookie("setedit", 'CKG', '365', '/');
 
 
 }
@@ -39,20 +44,39 @@ function set_dw_edit() {
    //expire setedit cookie, if any
    //create new setedit cookie with DW edit as the preference
   
-   eraseCookie("setedit");
-   createCookie("setedit", 'DW', '365');
+   //console.log("in set_dw_edit");
+   eraseCookie("setedit", "/");
+   createCookie("setedit", 'DW', '365', '/');
 }
 
 
 jQuery(function() {
     
+   
+   //construct path
+    var ns=readCookie("FCK_NmSp");
+    var ns_arr=ns.split("%3A");
+    var new_ns="/";
+    for(var i=0;i < (ns_arr.length-1);i++) {
+      new_ns+=ns_arr[i];
+      if (i<(ns_arr.length-2)){
+        new_ns+="/";
+      }
+    }
+    //console.log("new_ns="+new_ns);
+
+      
     var editor=readCookie("setedit");
+    //console.log("editor="+editor);
     
     if (editor != null){
-      eraseCookie("FCKG_USE");
+      //erase cookie in the current name space
+     
+      eraseCookie("FCKG_USE", "new_ns");
+      eraseCookie("FCKG_USE", "/");
       if (editor=='DW') {
-        createCookie("FCKG_USE", "_false_", 0);
-      }
+        createCookie("FCKG_USE", "_false_", 0, "/");
+      } 
 
     }
     
